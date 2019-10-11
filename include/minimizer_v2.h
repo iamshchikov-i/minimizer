8bdb6b2d4304f22c433709e306e06598c0c27346
Ivan Iamshchikov <www.vano565@gmail.com>
2019-10-11 22:39:42 +0300

    get R_Max via priority queue

--- include/minimizer_v2.h
+++ include/minimizer_v2.h
@@ -3,14 +3,34 @@
 
 #include <cmath>
 #include <map>
+#include <queue>
 #include <algorithm>
 #include <iostream>
 
-struct result
-{
+struct result {
 	int k;
 	double x;
 	double y;
+};
+
+struct characteristics {
+	double y;
+	double R;
+	characteristics(double _y, double _R);
+	characteristics(double _R);
+};
+
+struct interval {
+	double first_point;
+	double second_point;
+	characteristics _ch;
+	interval(double _f_point, double _s_point, double _R);
+};
+
+struct CompareR {
+	bool operator()(interval const& i1, interval const& i2) {
+		return i1._ch.R < i2._ch.R;
+	}
 };
 
 class Minimizer_v2 {
@@ -23,24 +43,30 @@ private:
 	double eps; 
 	int N_max;
 	double min_interval_length;
-	std::map<double, double>* values; 
-	std::map<double, double>::iterator left_point;
-	std::map<double, double>::iterator right_point;
+	std::map<double, characteristics>* values;
+	std::map<double, characteristics>::iterator left_point;
+	std::map<double, characteristics>::iterator right_point;
+	std::priority_queue<interval, std::vector<interval>, CompareR>* pq;
 	double(*function) (double _x);
 protected:
 	bool stop1();
 	bool stop2();
+	bool isEnd();
 	void go_Next_Interval();
+	void go_new_left_interval(double new_point);
 	void reset();
 	double get_M();
 	double get_M_Max();
-	double get_R(double m);
+	double get_R();
+	void calculate_R(double new_point, double new_m);
 	double get_m();
-	std::pair<double, double> find_R_Max(double m);
-	double get_new_point(std::pair<double, double> p, double m);
+	void insert_to_map(double _x, double _y, double _R);
+	void compare_interval_len(double new_point);
+	double get_new_point(interval i);
+	void delete_containers();
 public:
-	Minimizer_v2(double _a, double _b, double (*f)(double x), double _eps = 0.001, int _N_max = 500, double _r_par = 2.0);
-	void set_experiment(const double _a, const double _b, double(*f)(double x), const double _eps = 0.01, const int _N_max = 1000, const double _r_par = 1.5);
+	Minimizer_v2(double _a, double _b, double (*f)(double x), double _eps = 0.01, int _N_max = 500, double _r_par = 2.0);
+	void set_experiment(const double _a, const double _b, double(*f)(double x), const double _eps = 0.01, const int _N_max = 500, const double _r_par = 2.0);
 	result solve();
 };
 
--- src/minimizer_v2.cpp
+++ src/minimizer_v2.cpp
@@ -1,9 +1,16 @@
 #include "minimizer_v2.h"
 
+characteristics::characteristics(double _y, double _R): y(_y), R(_R) {}
+
+characteristics::characteristics(double _R): R(_R) {}
+
+interval::interval(double _f_point, double _s_point, double _R) : first_point(_f_point), second_point(_s_point), _ch(_R) {}
+
 Minimizer_v2::Minimizer_v2(double _a, double _b, double(*f)(double x), double _eps, int _N_max, double _r_par): a(_a), b(_b), function(f),
 																										eps(_eps), N_max(_N_max), r_p(_r_par) {
 	res.k = 0;
-	values = new std::map<double, double>;
+	values = new std::map<double, characteristics>;
+	pq = new std::priority_queue<interval, std::vector<interval>, CompareR>;
 }
 
 bool Minimizer_v2::stop1() {
@@ -14,24 +21,31 @@ bool Minimizer_v2::stop2() {
 	return min_interval_length <= eps;
 }
 
-void Minimizer_v2::reset() {
-	left_point = values->begin();
-	right_point = left_point; right_point++;
+bool Minimizer_v2::isEnd() {
+	return stop1() || stop2();
 }
 
 void Minimizer_v2::go_Next_Interval() {
 	left_point++; right_point++;
 }
 
+void Minimizer_v2::go_new_left_interval(double new_point) {
+	right_point = values->find(new_point);
+	left_point = right_point; left_point--;
+}
+
+void Minimizer_v2::reset() {
+	left_point = values->begin();
+	right_point = left_point; right_point++;
+}
+
 double Minimizer_v2::get_M() {
-	return abs(((*right_point).second - (*left_point).second) / ((*right_point).first - (*left_point).first));
+	return abs(((*right_point).second.y - (*left_point).second.y) / ((*right_point).first - (*left_point).first));
 }
 
 double Minimizer_v2::get_M_Max() {
 	double M_max, tmp;
 	reset();
-	double interval_length = (*right_point).first - (*left_point).first;
-	min_interval_length = interval_length;
 	M_max = get_M();
 	go_Next_Interval();
 
@@ -39,17 +53,35 @@ double Minimizer_v2::get_M_Max() {
 		tmp = get_M();
 		if (tmp >= M_max)
 			M_max = tmp;
-
-		interval_length = (*right_point).first - (*left_point).first;
-		if (interval_length < min_interval_length)
-			min_interval_length = interval_length;
 	}
 	return M_max;
 }
 
-double Minimizer_v2::get_R(double m) {
+double Minimizer_v2::get_R() {
 	double tmp = m * ((*right_point).first - (*left_point).first);
-	return tmp + (pow((*right_point).second - (*left_point).second, 2) / tmp) - 2 * ((*right_point).second + (*left_point).second);
+	return tmp + (pow((*right_point).second.y - (*left_point).second.y, 2) / tmp) - 2 * ((*right_point).second.y + (*left_point).second.y);
+}
+
+void Minimizer_v2::calculate_R(double new_point, double new_m) {
+	if (new_m != m) {
+		if (!pq->empty()) {
+			delete pq;
+			pq = new std::priority_queue<interval, std::vector<interval>, CompareR>;
+		}
+		m = new_m;
+		reset();
+		for (; right_point != values->end(); go_Next_Interval()) {
+			(*left_point).second.R = get_R();
+			pq->push(interval((*left_point).first, (*right_point).first, (*left_point).second.R));
+		}
+	}
+	else {
+		go_new_left_interval(new_point);
+		for (int i = 0;i < 2;++i, go_Next_Interval()) {
+			(*left_point).second.R = get_R();
+			pq->push(interval((*left_point).first, (*right_point).first, (*left_point).second.R));
+		}
+	}
 }
 
 double Minimizer_v2::get_m() {
@@ -62,28 +94,34 @@ double Minimizer_v2::get_m() {
 		throw - 1;
 }
 
-std::pair<double, double> Minimizer_v2::find_R_Max(double m) {
-	std::pair<double, double> res;
-	double R_max, tmp;
-	reset();
-
-	res = std::pair<double, double>((*left_point).first, (*right_point).first);
-	R_max = get_R(m);
-	go_Next_Interval();
-
-	for (; right_point != values->end(); go_Next_Interval()) {
-		tmp = get_R(m);
-		if (tmp >= R_max) {
-			R_max = tmp;
-			res.first = (*left_point).first;
-			res.second = (*right_point).first;
-		}
+void Minimizer_v2::insert_to_map(double _x, double _y, double _R) {
+	characteristics _ch(_y, _R);
+	values->insert({ _x, _ch });
+	if (_y < res.y) {
+		res.x = _x;
+		res.y = _y;
 	}
-	return res;
 }
 
-double Minimizer_v2::get_new_point(std::pair<double, double> p, double m) {
-	return 0.5*(p.first + p.second) - ((*function)(p.second) - (*function)(p.first)) / (2 * m);
+void Minimizer_v2::compare_interval_len(double new_point) {
+	go_new_left_interval(new_point);
+	double interval_length;
+	for (int i = 0;i < 2;++i, go_Next_Interval()) {
+		interval_length = (*right_point).first - (*left_point).first;
+		if (interval_length < min_interval_length)
+			min_interval_length = interval_length;
+	}
+}
+
+double Minimizer_v2::get_new_point(interval i) {
+	return 0.5*(i.first_point + i.second_point) - ((*function)(i.second_point) - (*function)(i.first_point)) / (2 * m);
+}
+
+void Minimizer_v2::delete_containers() {
+	delete values;
+	values = nullptr;
+	delete pq;
+	pq = nullptr;
 }
 
 void Minimizer_v2::set_experiment(double _a, double _b, double(*f)(double x),
@@ -96,41 +134,33 @@ void Minimizer_v2::set_experiment(double _a, double _b, double(*f)(double x),
 	r_p = _r_par;
 	res.k = 0;
 	if(values == nullptr)
-		values = new std::map<double, double>;
-	else throw -1;
+		values = new std::map<double, characteristics>;
+	if(pq == nullptr)
+		pq = new std::priority_queue<interval, std::vector<interval>, CompareR>;
 }
 
 result Minimizer_v2::solve() {
 	std::pair<double, double> new_point;
-	double _min;
+	double new_m;
 
 	//1я итерация
 	min_interval_length = b - a;
-	values->insert(std::pair<double, double>(a, (*function)(a)));
-	values->insert(std::pair<double, double>(b, (*function)(b)));
-	if ((*function)(a) <= (*function)(b)) {
-		res.x = a;
-		_min = (*function)(a);
-	}
-	else {
-		res.x = b;
-		_min = (*function)(b);
-	}		
+	insert_to_map(a, (*function)(a), 0);
+	res.x = a; res.y = (*function)(a);
+	insert_to_map(b, (*function)(b), 0);
+	m = -1;
+	pq->push(interval(a, b, 0));
 	res.k = 2;
-	
-	while (!stop1()) {
-		m = get_m();
-		if (stop2()) break;
-		new_point.first = get_new_point(find_R_Max(m), m); 
+
+	while (!isEnd()) {
+		new_m = get_m();
+		calculate_R(new_point.first, new_m);
+		new_point.first = get_new_point(pq->top()); pq->pop();
 		new_point.second = (*function)(new_point.first); 
-		values->insert(new_point); 
+		insert_to_map(new_point.first, new_point.second, 0);
 		res.k++; 
-		if (new_point.second <= _min) {
-			res.x = new_point.first;
-			res.y = new_point.second;
-		}
+		compare_interval_len(new_point.first);
 	}
-	delete values;
-	values = nullptr;
+	delete_containers();
 	return res;
 }
